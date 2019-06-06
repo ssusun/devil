6d0fbf408336295aa97c92ec09df7ecf6a8c4385
sunyuqin <sunyuqin@antiy.cn>
2019-06-06 17:03:24 +0800

    Add client/server lab code
    Signed-off-by: sunyuqin <sunyuqin@antiy.cn>

---
+++ client_server/ClientMain.cpp
@@ -0,0 +1,16 @@
+/*************************************************************************
+ @Author: Sun Yuqin
+ @Created Time : Thu 06 Jun 2019 04:57:18 PM CST
+ @File Name: ClientMain.cpp
+ @Description:
+ ************************************************************************/
+#include "Client.h"
+
+// 客户端主函数
+// 创建客户端对象后启动客户端
+int main(int argc, char *argv[]) {
+    Client client;
+    client.Start();
+    return 0;
+}
+
